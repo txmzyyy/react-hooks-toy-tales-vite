f0481fe18a864bf5711e810d1a7ebf09811d172f
txmzyyy <tamaramk08@gmail.com>
2026-05-17 21:53:53 +0300

    complete toy card features

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 
 function ToyCard({ toy, onDeleteToy, onLikeToy }) {
+  
+  function handleDelete() {
+  fetch(`http://localhost:3001/toys/${toy.id}`, {
+    method: "DELETE",
+  
+  })
+    .then((response) => response.json())
+    .then(() => {onDeleteToy(toy.id)});
+  }
+
+    function handleLike() {
+      fetch(`http://localhost:3001/toys/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          likes: toy.likes + 1,
+        }),
+      })
+        .then((response) => response.json())
+        .then((updatedToy) => onLikeToy(updatedToy));
+    }
+
   return (
     <div className="card" data-testid="toy-card">
       <h2>{toy.name}</h2>
