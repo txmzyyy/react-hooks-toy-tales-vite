1d5812b1bdb1fe4b0cbfe1a2a6650ba1fb82a836
txmzyyy <tamaramk08@gmail.com>
2026-05-17 21:45:17 +0300

    add toycard display features

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onDeleteToy, onLikeToy }) {
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button className="like-btn" onClick={() => onLikeToy(toy.id)}>
+        Like {"<3"}
+      </button>
+      <button className="del-btn" onClick={() => onDeleteToy(toy.id)}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
